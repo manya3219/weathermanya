8dea9ccdfb7de89c8593b5ba8bda1d78df3d80b1
manya_3219 <yadavmanya1902@gmail.com>
2023-10-27 14:34:53 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "d01e0995df4700632ce6b6face576879",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
